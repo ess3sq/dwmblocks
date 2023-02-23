d18f5e7c35cc4e3f8d2a913081c550695f128ed7
Lorenzo Calza <lorenzo@lorenzocalza.com>
2023-02-23 18:59:45 +0100

    Add changes made for the jellyfish version of dwmblocks

--- blocks.h
+++ blocks.h
@@ -1,15 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	// mpstat | awk '/all/ {print $3"%"}'
-
-	{" 🗂️ ", "mpstat | awk '/all/ {print$3\"%\"}'" ,                       30,  0},							  
-	{" 💻 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	  0},
-	{" ", "python3 /home/lorenzo/bin/battery_status.py",	             5,	  0},
-	{" 📅 ", "date '+%b %d (%a)  🕐 %H:%M '",						     5,	  0},
+	{" 🔊 ", "/home/lori/.dwm/bar_volume_level.py",                            1,         0},
+	{" 📶 ", "/home/lori/.dwm/wifi_name.sh",                                  10,         0},
+	{" 🌡 ", "sensors | grep \"Tccd1\" | awk -F \"+\" '{print $2}'",         5*60,         0},
+	{"💻 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",       30,         0},
+	{" 📅 ", "date '+%b %d (%a)  🕐 %H:%M '",						          10,         0},
 };
-
-// upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk 'FNR==11 || FNR==20' | cut -d" " -f15,20 | xargs -n2 -d'\n'
 							   
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
